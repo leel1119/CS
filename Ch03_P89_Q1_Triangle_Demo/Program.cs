720d1005b5adb13cf9081e37031ff09f349e2e80
leel1119 <leel1119@gmail.com>
2023-06-01 16:24:45 +0800

    20230601

---
+++ Ch03_P89_Q1_Triangle_Demo/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_P89_Q1_Triangle_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j;
+
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------------------------------------");
+            for (i = 0;i < 5; i++) 
+            {
+                for(j = 0; j < 5; j++) 
+                {
+                    if (j < (4-i))
+                        Console.Write(" ");                    
+                    else 
+                        Console.Write("*");
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------------------------------------");
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    if (j < i)
+                        Console.Write(" ");
+                    else
+                        Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("--------------------------------------------------");
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    if (j < (5 - i))
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Ch03_Test02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_Test02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int s;
+            s=0;
+
+            for (int i = 1; i <= 29; i = i+2) 
+            {
+                s = i * i;
+                sum = sum + s;
+            }
+            Console.WriteLine(sum);
+
+            int number = 1;
+            double square;
+            double total = 0;
+            for (int i = 0;i < 15; i++) 
+            {
+                square = Math.Pow(number, 2);
+                number = number + 2;
+                total = total + square;
+            }
+            Console.WriteLine(total);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
