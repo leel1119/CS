5cd47b22e5d937b9a063a8dd83dc3783d16c3645
leel1119 <leel1119@gmail.com>
2023-06-05 16:13:56 +0800

    20230605

---
+++ Ch03_P108_PassArray_Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_P108_PassArray_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] someNums = { 10, 21, 32, 45 };
+            MethodGetOneInetValue(someNums);
+            foreach (int i in someNums)
+            {
+                Console.WriteLine($"Each Element in Array : {i}");
+            }
+            Console.ReadLine();
+        }
+
+        private static void MethodGetOneInetValue(int[] theArray)
+        {
+            for (int idx=0; idx < theArray.Length; idx++)
+            {
+                Console.WriteLine($"Array Element In: {theArray[idx]}");
+                theArray[idx] = 999;
+                Console.WriteLine($"Array Element Out :{theArray[idx]}");
+
+            }
+        }
+    }
+}
---
+++ Ch03_P114_Ref_Returnned_Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_P114_Ref_Returnned_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] items = { 11, 2, 35, 40, 42 };
+            int itemToFind;
+            foreach (int i in items) Console.Write(i + "\t");      
+            Console.WriteLine("\n Please Enter the value to be found : ");
+            itemToFind = Convert.ToInt32(Console.ReadLine());
+            ref int foundedItem = ref FindItem(itemToFind, items);
+            foundedItem = 0;
+            foreach (int n in items) Console.Write(n + "\t");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        static ref int FindItem(int findValue, int[] theArray)
+        {
+            int pos = -1;
+            int idx = 0;
+            for (idx = 0; idx < theArray.Length; idx++) 
+            {
+                if (findValue == theArray[idx]) break;
+            }
+            return ref theArray[idx];
+        }
+    }
+}
